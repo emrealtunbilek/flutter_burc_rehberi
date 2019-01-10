17eb23148f48848102ccb663091a7977fe01578b
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-10 21:33:57 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_burc_rehberi/burc_liste.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: "Burc Rehberi",
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: BurcListesi(),
+    );
+  }
+
+}
